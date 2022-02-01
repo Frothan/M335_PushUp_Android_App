7be0be2631f9d7cd6210ef8d1cd2bb6e13b40447
chr1st0jr <christo.ch@hotmail.com>
2022-02-01 11:48:10 +0100

    Layout 1 done

---
+++ app/src/main/java/com/example/pushup/ShowHighscore.java
@@ -0,0 +1,19 @@
+package com.example.pushup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ShowHighscore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_highscore);
+    }
+
+    public void showHome(View view){
+
+    }
+}
