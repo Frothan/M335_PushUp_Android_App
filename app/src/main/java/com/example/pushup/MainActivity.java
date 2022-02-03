7dc51b333e7f88c6bb4579404e1d85712b790a32
Bismillah Frothan <bismillah.frothan33@gmail.com>
2022-02-03 17:15:15 +0100

    laufende stand

--- app/src/main/java/com/example/pushup/MainActivity.java
+++ app/src/main/java/com/example/pushup/MainActivity.java
@@ -1,23 +1,27 @@
 package com.example.pushup;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +31,11 @@ public class MainActivity extends AppCompatActivity {
     EditText eWeight;
     EditText eHeight;
     EditText eNumberOfPushups;
+    Button continueBtn;
+
+    User currentUser = new User();
+
+    ArrayList<User> userList = new ArrayList<User>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +43,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void saveData(View view) throws JSONException {
+
+
+  /*  public void saveData(View view) throws JSONException {
 
         eName = (EditText) findViewById(R.id.Name);
         eGoal = (EditText) findViewById(R.id.Goal);
@@ -44,48 +55,79 @@ public class MainActivity extends AppCompatActivity {
         eNumberOfPushups = (EditText) findViewById(R.id.Goal);
         //Muss noch geändert werden -> weil number of pushup ist der counter
 
-
-
-        User currentUser = new User();
+        currentUser.setId(6);
         currentUser.setFullName(eName.getText().toString());
         currentUser.setGoal(eGoal.getText().toString());
         currentUser.setAge(eAge.getText().toString());
         currentUser.setWeight(eWeight.getText().toString());
         currentUser.setHeight(eHeight.getText().toString());
         currentUser.setNumberOfPushups(eNumberOfPushups.getText().toString());
-        FileOutputStream outputStream;
-        JSONObject user = new JSONObject();
+
         try {
-            user.put("FullName", currentUser.getFullName());
-            user.put("Goal", currentUser.getGoal());
-            user.put("Age", currentUser.getAge());
-            user.put("Weight", currentUser.getWeight());
-            user.put("Height", currentUser.getHeight());
-            user.put("PushUp", currentUser.getNumberOfPushups());
-            user.put("Time", currentUser.getTime());
-            JSONArray jsonArray = new JSONArray();
-            jsonArray.put(user);
+            // Read json file
+            Context context = this;
+            File file = new File(context.getFilesDir(),"UserData.json");
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = bufferedReader.readLine();
+            while (line != null){
+                stringBuilder.append(line).append("\n");
+                line = bufferedReader.readLine();
+            }
+            String responce = stringBuilder.toString();
+            //
+
+            JSONObject obj = new JSONObject(responce);
+            // fetch JSONArray named users
+            JSONArray userArray = obj.getJSONArray("User");
+
+            bufferedReader.close();
+
+
+
+            userArray.put(currentUser.toJSON());
             JSONObject userObj = new JSONObject();
-            userObj.put("User", jsonArray);
-            String jsonStr = userObj.toString();
-            System.out.println("jsonString: "+jsonStr);
-            outputStream = openFileOutput("UserData", Context.MODE_PRIVATE);
-            outputStream.write(jsonStr.getBytes());
-            outputStream.close();
-            startPushUp(view);
+            userObj.put("User", userArray);
+            responce = userObj.toString();
+
+            // Write json file
+            // Define the File Path and its Name
+            FileWriter fileWriter = new FileWriter(file);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(responce);
+            bufferedWriter.close();
+
+
+            *//*startPushUp(view);*//*
         } catch (JSONException | FileNotFoundException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
+*/
 
     public void startPushUp(View view){
+        eGoal = (EditText) findViewById(R.id.Goal);
+        eName = (EditText) findViewById(R.id.Name);
+        eAge = (EditText) findViewById(R.id.Age);
+        eWeight = (EditText) findViewById(R.id.Weight);
+        eHeight = (EditText) findViewById(R.id.Height);
+
         Intent intent = new Intent(this, CounterActivity.class);
         String message = eGoal.getText().toString();
         intent.putExtra("goal", message);
+        User currentUser = new User();
+        currentUser.setId(2);
+        currentUser.setFullName(eName.getText().toString());
+        currentUser.setGoal(eGoal.getText().toString());
+        currentUser.setAge(eAge.getText().toString());
+        currentUser.setWeight(eWeight.getText().toString());
+        currentUser.setHeight(eHeight.getText().toString());
+
+        intent.putExtra("user", (Parcelable) currentUser);
         startActivity(intent);
     }
 }
